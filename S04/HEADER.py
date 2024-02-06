2c576bc7fd1ed84b5574b73f4284c66c8250dd23
dulcegar <d.garciai.2023@alumnos.urjc.es>
2024-02-06 16:27:00 +0100

    Adding S02 folder

---
+++ S04/HEADER.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+#--Constant with the new of the file to open
+FILENAME = "sequences/RNU6_269P.txt"
+
+#--Open and read the file
+file_contents = Path(FILENAME).read_text()
+
+list_contents = file_contents.split("\n")
+
+#--Print the header on the console
+print(list_contents[0])
