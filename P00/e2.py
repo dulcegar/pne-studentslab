86fb9ef3899750b6052a33a4cc9dcbca960edaec
dulcegar <d.garciai.2023@alumnos.urjc.es>
2024-02-07 18:34:51 +0100

    Adding S02 folder

--- P00/e2.py
+++ P00/e2.py
@@ -1,5 +1,7 @@
 from seq0 import *
 
-
-SEQUENCES = "../sequences/"
-GENES = ["U5", "ADA", "FRAT1", "FXN"]
+folder = "../sequences/"
+filename = "U5"
+print("DNA file: ", filename)
+body = seq_read_fasta(folder + filename)
+print("These are the first 20 bases", body[0:20])
