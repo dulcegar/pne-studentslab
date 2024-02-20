c507c03bc08767fe12bd6398b5bb0976298ae8c5
dulcegar <d.garciai.2023@alumnos.urjc.es>
2024-02-20 16:30:22 +0100

    Adding S02 folder

--- S07/car2.py
+++ S07/car2.py
@@ -1,18 +1,23 @@
-class car:
+class vehicle:   #this is the MOTHER class
+    def set_speed(self, speed):
+        self.speed = speed
+
+class car(vehicle):
     #  init is when you move an object to a class.
     def __init__(self, brand, speed = 0): #self is a parameter of all methos and it is always a parameter. It is the object of a class
         self.car_brand = brand
         self.speed = speed
 
 
-    def set_speed(self, speed):
-        self.speed = speed
-
 class Ferrari(car):
-    pass
+    def make_cabrio(self):
+        self.speed = 20
+        self.music = "loud"
+        return "Wow"
 
 mycar = car("Renault")
-yourcar = Ferrari("Ferrari")
+yourcar = Ferrari("Ferrari")   # ---> __init__
 print(yourcar.car_brand)
 yourcar.set_speed(120)
-print(yourcar.speed)
+print(yourcar.speed)  #ferrari no tiene speed definido pero car si entcs si en ferrari no esta, va a car a mirar y sino a vehicle porq dependen d ellas
+print(yourcar.make_cabrio(), "and music is", yourcar.music, "and speed is", yourcar.speed)
