668a18a8b725b58ec769e91609775fe948feb88b
dulcegar <d.garciai.2023@alumnos.urjc.es>
2024-02-21 18:19:25 +0100

    Adding S02 folder

--- S06/e1.py
+++ S06/e1.py
@@ -1,10 +1,20 @@
 class Seq:
+
     def __init__(self, strbases):
+        bases = ["A", "C", "T", "G"]
+        for b in strbases:
+            if b not in bases:
+                self.strbases = "ERROR!"
+                print("Incorrect sequence detected")
+                return
+
         self.strbases = strbases
         print("New sequence created!")
 
     def __str__(self):   #sirve para devolver en una cadena de caracteres la representacion de mi objeto
         return self.strbases   #devolvemos lo que vale el atributo, basicamente el ATGC, en este caso sin adornos
+
+    def print_seqs(seq_list):
 
 
 s1 = Seq("ACCTGC")
