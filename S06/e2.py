9182fe675685049f5ac36628b48c912e34ff7798
dulcegar <d.garciai.2023@alumnos.urjc.es>
2024-02-27 11:45:17 +0100

    commit message

--- S06/e2.py
+++ S06/e2.py
@@ -2,5 +2,10 @@ from seq import Seq
 
 seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
 
+'''
 for s in seq_list:
     print(f"Sequence {seq_list.index(s)}: (Length: {s.len()}) {s}")
+'''
+#otra forma
+for i, s in enumerate(seq_list):   #enumerate crea una lista nueva y la enumera empezando por 0
+    print(f"Sequence {i}: (Length: {s.len()}) {s}")
