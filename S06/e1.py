a65307691362481920ec8549dac5a1a5b8d54a92
dulcegar <d.garciai.2023@alumnos.urjc.es>
2024-02-27 11:30:52 +0100

    commit message

--- S06/e1.py
+++ S06/e1.py
@@ -1,21 +1,5 @@
-class Seq:
-
-    def __init__(self, strbases):
-        bases = ["A", "C", "T", "G"]
-        for b in strbases:
-            if b not in bases:
-                self.strbases = "ERROR!"
-                print("Incorrect sequence detected")
-                return
-
-        self.strbases = strbases
-        print("New sequence created!")
-
-    def __str__(self):   #sirve para devolver en una cadena de caracteres la representacion de mi objeto
-        return self.strbases   #devolvemos lo que vale el atributo, basicamente el ATGC, en este caso sin adornos
-
-    def print_seqs(seq_list):
-
+#nos pide cambiar en el seq-01 e impprtarlo aqui para hacer la funcion
+from seq import Seq
 
 s1 = Seq("ACCTGC")
 s2 = Seq("Hello? Am I a valid sequence?")
