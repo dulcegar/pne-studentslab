0c7eb195364d5bd076dbf38b87ce1ce43dad733b
dulcegar <d.garciai.2023@alumnos.urjc.es>
2024-03-06 12:27:45 +0100

    commit message

--- P02/client0.py
+++ P02/client0.py
@@ -1,33 +1,41 @@
 import socket
 
 class Client:
-    def __init__(self, IP, PORT):
-        self.IP = IP
-        self.PORT = PORT
+    def __init__(self, SERVER_IP: str, SERVER_PORT: int):  #ese ip y ese puerto son del servidor
+        self.SERVER_IP = SERVER_IP
+        self.SERVER_PORT = SERVER_PORT  #nos almacenamos dentro del cliente el ip y el puerto del servidor
 
     def ping(self):
         print("Ok!")
 
-    def __str__(self):
-        return f"Connection to SERVER at {self.IP}, PORT: {self.PORT}"
+    def __str__(self): #en el str no hace falta poner mas que self
+        return f"Connection to SERVER at {self.SERVER_IP}, PORT: {self.SERVER_PORT}"
 
     def talk(self, msg):
+        import socket #se puede poner tanto arriba como dentro de la funcion
         # -- Create the socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         # establish the connection to the Server (IP, PORT)
-        s.connect((self.IP, self.PORT))
+        client_socket.connect((self.SERVER_IP, self.SERVER_PORT)) #doble parentesis porq es una tupla
+
+        #convertimos en 0 y 1 el mensaje que nos llega
+        msg_bytes = str.encode(msg)
 
         # Send data.
-        s.send(str.encode(msg))
+        client_socket.send(msg_bytes)
 
         # Receive data
-        response = s.recv(2048).decode("utf-8")
+        response_bytes = client_socket.recv(2048) #es la capacidad maxima de mi server
+
+        #convertir los bytes en mensaje
+        response = response_bytes.decode("utf-8") #no hace falta poner el utf-8 xqe viene por defecto
 
         # Close the socket
-        s.close()
+        client_socket.close()
 
         # Return the response
         return response
 
 
+c = Client("127.0.0.1", 8081) #ejemplo d crearse un cliente
--- P02/e1.py
+++ P02/e1.py
@@ -6,7 +6,7 @@ EXERCISE = 1
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 # -- Parameters of the server to talk to
-IP = "212.128.255.97" # your IP address
+IP = "212.128.255.97"  # your IP address, pa este ejercicio nos da igual xqe no nos conectamos a ningun server
 PORT = 8081
 
 # -- Create a client object
