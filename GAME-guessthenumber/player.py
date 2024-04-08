a915283660260c2ac0d4d463af16ee6c073a43dd
dulcegar <d.garciai.2023@alumnos.urjc.es>
2024-04-08 15:32:30 +0200

    Adding S02 folder

--- GAME-guessthenumber/player.py
+++ GAME-guessthenumber/player.py
@@ -4,7 +4,6 @@ import termcolor
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8080
 
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_IP, SERVER_PORT))
 
